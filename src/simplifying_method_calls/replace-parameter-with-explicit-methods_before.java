6296ef02aa0b0902a1e8df198d4a342509f704e2
César Salcedo <cesar.salcedo@utec.edu.pe>
2019-06-03 10:26:58 -0500

    Replaced setter that receives variable name with setters for each variable

--- src/simplifying_method_calls/replace-parameter-with-explicit-methods_before.java
+++ src/simplifying_method_calls/replace-parameter-with-explicit-methods_before.java
@@ -1,11 +1,7 @@
-void setValue(String name, int value) {
-  if (name.equals("height")) {
-    height = value;
-    return;
-  }
-  if (name.equals("width")) {
-    width = value;
-    return;
-  }
-  Assert.shouldNeverReachHere();
-}
+void setHeight(int value) {
+  height = value;
+} 
+
+void setWidth(int value) {
+  width = value;
+} 
