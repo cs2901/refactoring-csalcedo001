5eaa1e8905b925c701aa3aee613ec09839e56382
César Salcedo <cesar.salcedo@utec.edu.pe>
2019-06-03 10:15:28 -0500

    If-else structure into happy-path structure. The normal value is found at the end of the function

--- src/simplifying_conditional_expressions/replace-nested-conditional-with-guard-clauses_before.java
+++ src/simplifying_conditional_expressions/replace-nested-conditional-with-guard-clauses_before.java
@@ -1,18 +1,15 @@
 public double getPayAmount() {
-  double result;
-
   if (isDead) {
-    result = deadAmount();
-  }
-  else if (isSeparated) {
-  	result = separatedAmount();
-  }
-  else if (isRetired) {
-  	result = retiredAmount();
-  }
-  else {
-  	result = normalPayAmount();
+    return deadAmount();
   }
 
-  return result;
+  if (isSeparated) {
+  	return separatedAmount();
+  }
+  
+  if (isRetired) {
+  	return retiredAmount();
+  }
+
+  return normalPayAmount();
 }
